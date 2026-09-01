6e48d5e546253e0ac8465720805e5639d3ce26ac
mgcavero04 <mgcavero04@hotmail.com>
2026-09-01 18:09:43 -0400

    Fix database data type layout mismatch by removing nvarchar max

--- API/Data/Migrations/20260901220934_PostgresInitialCreate.Designer.cs
+++ API/Data/Migrations/20260901220934_PostgresInitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20260901220657_PostgresInitialCreate")]
+    [Migration("20260901220934_PostgresInitialCreate")]
     partial class PostgresInitialCreate
     {
         /// <inheritdoc />
